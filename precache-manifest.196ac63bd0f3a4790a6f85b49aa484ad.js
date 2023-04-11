39b43e5f4cd42993ed1796748803d2e8934e2818
Anurag Jain <itsanuragjain.03@gmail.com>
2023-04-12 01:10:50 +0530

    Updates

---
+++ precache-manifest.196ac63bd0f3a4790a6f85b49aa484ad.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "dadc0e70ee1e16a06945",
+    "url": "/loginpage/static/js/main.d52c06b0.chunk.js"
+  },
+  {
+    "revision": "a31d604db3f3c869db33",
+    "url": "/loginpage/static/js/runtime~main.bb1b1f53.js"
+  },
+  {
+    "revision": "0cf31ef60762fd7a30a8",
+    "url": "/loginpage/static/js/2.9151ddd8.chunk.js"
+  },
+  {
+    "revision": "f49e295b7d14e9671aed2f6263af36fe",
+    "url": "/loginpage/index.html"
+  }
+];
